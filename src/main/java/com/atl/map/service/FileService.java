7b05f406caf08914accee0c895e4b5ea5f6a7d6b
PanicAthe <panicathe@naver.com>
2026-04-12 16:04:01 +0900

    ♻️ refactor: 파일 API 응답 표준화

--- src/main/java/com/atl/map/service/FileService.java
+++ src/main/java/com/atl/map/service/FileService.java
@@ -1,10 +1,13 @@
 package com.atl.map.service;
 
 import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.atl.map.dto.response.file.UploadFileResponseDto;
 
 public interface FileService {
     
-    String upload(MultipartFile file);
-    Resource getImage(String filename);
+    ResponseEntity<? super UploadFileResponseDto> upload(MultipartFile file);
+    ResponseEntity<Resource> getImage(String filename);
 }
